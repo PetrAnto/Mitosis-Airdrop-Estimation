ce8d712739023d478d6013329c8b487b8e49c884
PetrAnto <petrantonft@gmail.com>
2025-04-29 20:39:53 +0200

    Update main.jsx chart.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';    // ← important !
 
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
