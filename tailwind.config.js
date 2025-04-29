0305cb9d5cbf52f909d042924647dac4f6032fa3
PetrAnto <petrantonft@gmail.com>
2025-04-29 18:49:46 +0200

    chore: configure Tailwind content and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-// Tailwind config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
