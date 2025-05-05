dd4866f4c2a28b68ac270b3dd0332c44c41085ab
PetrAnto <petrantonft@gmail.com>
2025-05-05 16:21:35 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// src/config.js
+export const API_EXPEDITION = import.meta.env.VITE_API_EXPEDITION;
+export const API_THEO      = import.meta.env.VITE_API_THEO;
+export const API_TESTNET   = import.meta.env.VITE_API_TESTNET;
