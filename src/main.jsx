bb7eac72e237626d6ae7c3057fcc9719b60e0669
PetrAnto <petrantonft@gmail.com>
2025-05-06 15:17:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';    // ← important !
+// ← Add this line
+import 'katex/dist/katex.min.css'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 import {
   Chart as ChartJS,
   ArcElement,
