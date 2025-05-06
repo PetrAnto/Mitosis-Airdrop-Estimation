c97e9382b136d2b76eea9190c18ed1c92d985538
PetrAnto <petrantonft@gmail.com>
2025-05-06 13:33:52 +0200

    Update CalculationDetails.jsx

--- src/components/CalculationDetails.jsx
+++ src/components/CalculationDetails.jsx
@@ -30,7 +30,7 @@ export default function CalculationDetails({
         </p>
         <img
           src="/public/expedition-basic-formula-1.png"
-          width="200"
+          width="100"
           alt="Expedition formula"
           className="w-full rounded mt-2 shadow-md"
         />
