4a9537138a7a33a493f09f016f7415fa4926bdfd
PetrAnto <petrantonft@gmail.com>
2025-05-07 09:38:11 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [
     require('@tailwindcss/typography'),
+    // …other plugins
   ],
 }
