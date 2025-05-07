4b2102f71aba9213e6d2c773723af0cf74244efc
PetrAnto <petrantonft@gmail.com>
2025-05-07 10:35:00 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    // if you have an index.html in your project root:
+    './index.html',
+    // **and** if you also have one under src/:
+    './src/index.html',
+
+    // everything else in src/ that uses tailwind classes/component JSX
+    './src/**/*.{js,jsx,ts,tsx,html}',
+
+    // if you ever in the future import or write .md/.mdx with classes:
+    //'./public/**/*.{html,md,mdx}'
+  ],
   darkMode: 'class',
   theme: { extend: {} },
   plugins: [
     require('@tailwindcss/typography'),
-    // …other plugins
+    // …any others
   ],
 }
