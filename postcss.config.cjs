54a607dc93979bc83c877eabf90e1fbf7cee3348
PetrAnto <petrantonft@gmail.com>
2025-05-07 16:36:47 +0200

    Delete postcss.config.cjs

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: {
-    // Use the standalone PostCSS plugin entrypoint:
-    '@tailwindcss/postcss': {},
-    // And autoprefixer for vendor prefixes:
-    autoprefixer: {},
-  }
-}
