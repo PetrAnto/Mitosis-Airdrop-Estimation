cf6afa8e9419610c8284187dfac7e8e5c7b4689a
PetrAnto <petrantonft@gmail.com>
2025-05-07 16:25:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
