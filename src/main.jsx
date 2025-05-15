cfa16c0e092b6ea61924a04c09e0775e13af38a3
PetrAnto <petrantonft@gmail.com>
2025-05-15 12:57:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// pull in your Tailwind-generated CSS:
+import './styles/global.css'
 
-// Global styles (Tailwind, etc.)
-import './styles/global.css';
-
-// Chart.js registration
 import {
   Chart as ChartJS,
   ArcElement,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from 'chart.js'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend)
 
-// Render the application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
