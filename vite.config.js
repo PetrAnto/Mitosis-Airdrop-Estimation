ba3afa5d9fcc2d7af9fbb02761f24a48dbc07b24
PetrAnto <petrantonft@gmail.com>
2025-05-15 11:53:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// this is Vite’s Tailwind plugin — not PostCSS’s:
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    // this is Vite’s Tailwind plugin — not PostCSS’s:
+    tailwind(),
   ],
   server: {
     proxy: {
