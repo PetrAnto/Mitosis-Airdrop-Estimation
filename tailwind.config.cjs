4f30f556fcdc3762df88d7eb2da4c0c60d9aa928
PetrAnto <petrantonft@gmail.com>
2025-05-15 12:56:56 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './public/**/*.{html,md}',
-    './src/**/*.{js,jsx,ts,tsx,html}',
+    './index.html',                  // your root HTML
+    './public/**/*.{html,md}',       // anything you drop in public/
+    './src/**/*.{js,jsx,ts,tsx,html}', // *all* your React components
   ],
-  darkMode: 'class',
-  theme: { extend: {} },
+  darkMode: 'class',  
+  theme: {
+    extend: {},
+  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
