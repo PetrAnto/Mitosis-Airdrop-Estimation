e66dace9e974462a042b9e11a4aca785e2f02b73
PetrAnto <petrantonft@gmail.com>
2025-05-21 13:14:35 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,4 +16,10 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
   ],
+  safelist: [
+  'bg-black', 'text-white', 'bg-gray-700', 'bg-gray-800', 'bg-gray-900',
+  'text-gray-200', 'text-gray-300', 'text-gray-400', 'text-red-500', 
+  'rounded-2xl', 'rounded-md', 'shadow-lg', 'font-sans', 'font-semibold', 'font-bold',
+  'accent-blue-500', 'focus:ring-2', 'focus:ring-blue-500', 'placeholder-gray-500'
+],
 }
