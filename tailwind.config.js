5f2ba4bdbc4d7e9dcee895c16cfbcff14c99f67b
PetrAnto <petrantonft@gmail.com>
2025-05-22 16:55:17 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,36 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: ['bg-test123'],
-  theme: { extend: {} },
-  plugins: [],
+  content: [
+    "./index.html",
+    "./public/**/*.{html,md}",
+    "./src/**/*.{js,jsx,ts,tsx,html}"
+  ],
+  safelist: [
+    "bg-black",
+    "bg-gray-900",
+    "bg-gray-800",
+    "bg-gray-700",
+    "bg-test123",
+    "text-white",
+    "text-gray-200",
+    "text-gray-300",
+    "text-gray-400",
+    "text-red-500",
+    "rounded-2xl",
+    "rounded-md",
+    "shadow-lg",
+    "font-sans",
+    "font-semibold",
+    "font-bold",
+    "accent-blue-500",
+    "focus:ring-2",
+    "focus:ring-blue-500",
+    "placeholder-gray-500"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require("@tailwindcss/typography")
+  ]
 }
