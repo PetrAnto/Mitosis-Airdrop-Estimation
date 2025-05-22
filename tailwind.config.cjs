af8bbed469a5efdaee035c3c35ed3051beeb8810
PetrAnto <petrantonft@gmail.com>
2025-05-22 16:10:36 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
     './src/**/*.{cjs,js,jsx,ts,tsx,html}',
   ],
   safelist: [
-    'bg-black', 'bg-gray-900', 'bg-gray-800', 'bg-gray-700', 'text-white',
+    'bg-black', 'bg-gray-900', 'bg-test123', 'bg-gray-800', 'bg-gray-700', 'text-white',
     'text-gray-200', 'text-gray-300', 'text-gray-400', 'text-red-500',
     'rounded-2xl', 'rounded-md', 'shadow-lg', 'font-sans', 'font-semibold', 'font-bold',
     'accent-blue-500', 'focus:ring-2', 'focus:ring-blue-500', 'placeholder-gray-500'
